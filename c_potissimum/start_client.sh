baf5b31a710caea48f65a0969f8c960a341ef7ba
evgeny <evgeny@outlook.com>
2014-06-11 16:00:49 +0400

    new file:   c_potissimum/_client 	updated client uses boost sockets  :  c_potissimum/client.cpp (under construction) 	modified:   c_potissimum/start_client.sh 	new file:   s_potissimum/_server 	updated server uses boost sockets and c++11 threads :   s_potissimum/server.cpp (under construction)    The necessary keys to compile both server and client : keys_to_compile_with

--- c_potissimum/start_client.sh
+++ c_potissimum/start_client.sh
@@ -1,7 +1,7 @@
 wget -P ./  https://ekondrat.web.cern.ch/ekondrat/.my_ip > /dev/null 2> /dev/null
 #./a.out `cat ./.my_ip`
 #./client `cat ./.my_ip`
-./client `cat ./.my_ip`
+./_client `cat ./.my_ip` $1
 rm -f .my_ip
 
 
